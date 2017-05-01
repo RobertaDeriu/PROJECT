85511b5a6813ed33c0601f3cbf6e08b423f3d3a8
DamienzGit <damianomails@gmail.com>
2017-05-01 22:24:31 +0200

    Aggiunto carattere al pinguino

--- src/Pinguino.java
+++ src/Pinguino.java
@@ -1,22 +1,14 @@
 import java.util.ArrayList;
 
-public class Pinguino extends Thread implements PinguinoInterface {
+public class Pinguino extends Thread{
 
     public String nome;
     public String sesso;
     public Pinguino partner;
+    Carattere carattere;
     public ArrayList<Pinguino> figli;
     public int eta;
     public int felicita;
 
 
-    @Override
-    public void trovaPartner(Graph ecosistema) {
-        
-    }
-
-    @Override
-    public void procrea() {
-
-    }
 }
